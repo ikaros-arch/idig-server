89de2098a00a70faa4f489512d619623a8280023
Hallvard Indgjerd <hallvard.in@gmail.com>
2025-04-25 22:51:31 +0300

    Update entrypoint script to use project name "KNS-Dhaskalio Project" for initialization

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 # Initialize the project and add a default user if not already done
 if [ ! -d "/data/K25" ]; then
-  ./idig-server create K25
-  ./idig-server adduser K25 admin manolis2016
+  ./idig-server create "KNS-Dhaskalio Project"
+  ./idig-server adduser "KNS-Dhaskalio Project" admin manolis2016
 fi
 
 # Start the server
