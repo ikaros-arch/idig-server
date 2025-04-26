5766bad66d38e059a5fb4ec9955c7e3469b4c50d
Hallvard Indgjerd <hallvard.in@gmail.com>
2025-04-26 14:32:34 +0300

    Refactor entrypoint script to use environment variables for project name and admin credentials

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Initialize the project and add a default user if not already done
-if [ ! -d "/data/KNS-Dhaskalio" ]; then
-  ./idig-server create "KNS-Dhaskalio"
-  ./idig-server adduser "KNS-Dhaskalio" admin manolis2016
+if [ ! -d "/data/$PROJECT_NAME" ]; then
+  ./idig-server create "$PROJECT_NAME"
+  ./idig-server adduser "$PROJECT_NAME" "$ADMIN_USER" "$ADMIN_PASSWORD"
 fi
 
 echo "Starting the server..."
